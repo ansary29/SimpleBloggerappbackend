2b87d1fade525d2107797c937e4cfcf8b515469c
Mohamed Ansary <85570638+ansary29@users.noreply.github.com>
2022-01-27 14:46:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const PostRoutes = require("./routes/PostRoutes.js");
-const PORT = 5000;
+const PORT =process.env.PORT|| 5000;
 
 const app = express();
 app.use(cors());
